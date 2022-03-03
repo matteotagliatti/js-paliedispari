c2c8b1100c1c1d87d23d0808daa18392abc094d1
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-03-03 13:45:19 +0100

    first commit. check palindrome done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const userStringOutput = document.getElementById("output-user-string");
+const palindromegOutput = document.getElementById("output-palindrome");
+
+const userString = prompt("Inserisci una parola").toLocaleLowerCase();
+
+userStringOutput.innerHTML = userString;
+palindromegOutput.innerHTML = checkPalindrome(userString);
+
+function checkPalindrome(string) {
+  const array = string.split(""); // Convert string to array
+  const reverseArray = array.reverse(); // Reverse the array
+  const reverseString = reverseArray.join(""); // Convert array to string
+
+  if (string == reverseString) {
+    return true;
+  } else {
+    return false;
+  }
+}
