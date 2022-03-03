eed5d4fa572179a3d226a8ba3b4e34b938cd023f
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-03-03 13:46:43 +0100

    add comment

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ const userString = prompt("Inserisci una parola").toLocaleLowerCase();
 userStringOutput.innerHTML = userString;
 palindromegOutput.innerHTML = checkPalindrome(userString);
 
+/**
+ * Check if user string is palindrome.
+ * @param {*} string // use string
+ * @returns
+ */
 function checkPalindrome(string) {
   const array = string.split(""); // Convert string to array
   const reverseArray = array.reverse(); // Reverse the array
