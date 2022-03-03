7f0ac39309e7495fc569d094e508ba6e9348bcf2
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-03-03 14:37:53 +0100

    pari e dispari complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+/*** PALINDROME ***/
+
 const userStringOutput = document.getElementById("output-user-string");
 const palindromegOutput = document.getElementById("output-palindrome");
 
-const userString = prompt("Inserisci una parola").toLocaleLowerCase();
+const userString = prompt("enter a word").toLocaleLowerCase();
 
 userStringOutput.innerHTML = userString;
 palindromegOutput.innerHTML = checkPalindrome(userString);
@@ -21,4 +23,71 @@ function checkPalindrome(string) {
   } else {
     return false;
   }
+}
+
+/*** PARI E DISPARI ***/
+
+const outputOddEven = document.getElementById("output-oddeven");
+const outputUserNumber = document.getElementById("output-usernumber");
+const outputComputerNumber = document.getElementById("output-computernumber");
+const outputSum = document.getElementById("output-sum");
+const whoWonOutput = document.getElementById("output-won");
+
+let inputSelect = prompt("even or odd?").toLocaleLowerCase();
+
+/* Check if inputSelect is different from Odd or Even */
+while (inputSelect !== "odd" && inputSelect !== "even") {
+  inputSelect = prompt("even or odd?").toLocaleLowerCase();
+}
+
+let userNumber = parseInt(prompt("enter a number beetween 1 and 5"));
+const computerNumber = randomNumber();
+
+/* Check if userNumber is a number and beetween 1 and 5 */
+while (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
+  userNumber = parseInt(prompt("enter a number beetween 1 and 5"));
+}
+
+const sum = userNumber + computerNumber;
+
+printDOM(outputOddEven, inputSelect);
+printDOM(outputUserNumber, userNumber);
+printDOM(outputComputerNumber, computerNumber);
+printDOM(outputSum, sum);
+
+/* Check if sum is even or odd */
+if (isEven(sum)) {
+  printDOM(whoWonOutput, "user");
+} else {
+  printDOM(whoWonOutput, "computer");
+}
+
+/**
+ * Random number beetween 1 and 5
+ * @returns
+ */
+function randomNumber() {
+  return Math.floor(Math.random() * 5 + 1);
+}
+
+/**
+ * Check if number is even
+ * @param {*} number
+ * @returns
+ */
+function isEven(number) {
+  if (number % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * Print element in the DOM
+ * @param {*} variable // variable name
+ * @param {*} value // value of the variable to be display in the DOM
+ */
+function printDOM(variable, value) {
+  variable.innerHTML = value;
 }
